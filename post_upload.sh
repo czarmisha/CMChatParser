2b7006c3b9d10f39331525ede6d0fffa61881f28
LexxITstorage <57769735+LexxITstorage@users.noreply.github.com>
2022-11-11 20:25:01 +0500

    Update post_upload.sh

--- post_upload.sh
+++ post_upload.sh
@@ -4,7 +4,7 @@ if systemctl stop watcher.service ; then
     sudo cp /home/ubuntu/cm_chat/dist/cm_chat /usr/bin/
 else
     echo "No all_telegram_bots.service found in system, now creating"
-    sudo cp /home/ubuntu/cm_chat/watcher.service /etc/systemd/system
+    sudo cp /home/ubuntu/cm_chat/cm_chat.service /etc/systemd/system
     sudo cp /home/ubuntu/cm_chat/dist/cm_chat /usr/bin/
 fi
 sudo systemctl daemon-reload
