6966bbfed7d4eb25ae3a60e059d0bcf5a5b201fc
LexxITstorage <57769735+LexxITstorage@users.noreply.github.com>
2022-11-11 20:36:35 +0500

    Update post_upload.sh

--- post_upload.sh
+++ post_upload.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 if systemctl stop cm_chat.service ; then
     sudo cp /home/ubuntu/cm_chat/cm_chat.service /etc/systemd/system/
-    sudo cp /home/ubuntu/cm_chat/dist/cm_chat /usr/bin/
+    sudo cp /home/ubuntu/cm_chat/dist/cm_chat /home/ubuntu/cm_chat
 else
     echo "No all_telegram_bots.service found in system, now creating"
     sudo cp /home/ubuntu/cm_chat/cm_chat.service /etc/systemd/system
-    sudo cp /home/ubuntu/cm_chat/dist/cm_chat /usr/bin/
+    sudo cp /home/ubuntu/cm_chat/dist/cm_chat /home/ubuntu/cm_chat
 fi
 sudo systemctl daemon-reload
 sudo systemctl enable cm_chat.service
